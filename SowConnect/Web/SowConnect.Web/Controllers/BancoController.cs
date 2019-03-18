a46cf5fa4bf1251d6d5e33d3614a62681493e567
Gabriel Ferreira <gabriel.leandrof@gmail.com>
2019-03-17 22:08:18 -0300

    scripts sql

---
+++ SowConnect/Web/SowConnect.Web/Controllers/BancoController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SowConnect.Web.Controllers
+{
+    public class BancoController : Controller
+    {
+        public IActionResult Banco()
+        {
+            return View();
+        }
+    }
+}
